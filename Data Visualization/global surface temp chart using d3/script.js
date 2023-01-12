4cff6b808a84b6bb0fdb23704f1e8c567e744b50
Vlad <37779807+vblanton@users.noreply.github.com>
2023-01-11 19:31:33 -0500

    finished adding color to each cell

--- Data Visualization/global surface temp chart using d3/script.js
+++ Data Visualization/global surface temp chart using d3/script.js
@@ -9,45 +9,79 @@ using this JSON file for the data:
 https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json
 
 Todo:
-cells
+Cells
+  function to calculate color of cell
 legend
-calculate color of cell via a function
+  use color function
+debug
+  why are my data points mapped upside down on the y axis
 
 Todo Optional:
-yaxis: Calculate the yaxis month name via a function call to reduce duplicity
+
 */
 
 d3.json ('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
   .then(data => {
 
-  // variables
+  // VARIABLES
 
   const w = 1200,
         h = 500,
         paddingLeft = 100,
-        paddingRight = 50,
+        paddingRight = 10,
         paddingTop = 10,
         paddingBottom = 100;
 
-  // svg container
+// temperature ranges for colors:
+// 2.8-3.9, 3.9-5.0, 5.0-6.1, 6.1-7.2, 7.2-8.3, 8.3-9.5, 9.5-10.6, 10.6-11.7, 11.7-12.8
+  let colors = [
+    "#4575b4",
+    "#74add1",
+    "#abd9e9",
+    "#e0f3f8",
+    "#ffffbf",
+    "#fee090",
+    "#fdae61",
+    "#f46d43",
+    "#d73027"
+  ]
+
+  // FUNCTIONS
+
+  function colorCalc (value) {
+    if (value < 3.9) {
+      return colors[0];
+    } else if (value >= 3.9 && value < 5.0) {
+      return colors[1];
+    } else if (value >= 5.0 && value < 6.1) {
+      return colors[2];
+    } else if (value >= 6.1 && value < 7.2) {
+      return colors[3];
+    } else if (value >= 7.2 && value < 8.3) {
+      return colors[4];
+    } else if (value >= 8.3 && value < 9.5) {
+      return colors[5];
+    } else if (value >= 9.5 && value < 10.6) {
+      return colors[6];
+    } else if (value >= 10.6 && value < 11.7) {
+      return colors[7];
+    } else if (value >= 11.7) {
+      return colors[8];
+    }
+  }
+
+  // SVG D3 CONTAINER
 
   let svg = d3.select('#svg')
     .append('svg')
     .attr('width', w)
-    .attr('height', h)
+    .attr('height', h);
 
   // x axis
-
-  // OLD: create an array for the x scale domain to use
-  // let xDomainArray = [];
-  // for (let i = 1753; i <= 2015; i++) {
-  //   xDomainArray.push(i);
-  // }
-  // xDomainArray = xDomainArray.filter((d,i) => !(i%15));  // filter to only show 1 in every 15 ticks
-
   // scaleBand() requires for the domain to be an array
+
   let xScale = d3.scaleBand()
-    .domain(data.monthlyVariance.map((d) => d.year)) // needs to be filtered
+    .domain(data.monthlyVariance.map((d) => d.year)) // filtered below in .tickValues()
     //.domain(xDomainArray)
     .range([0, w - paddingLeft - paddingRight]);
   let xAxis = d3
@@ -81,7 +115,7 @@ d3.json ('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/ma
 
   let yScale = d3
     .scaleBand()
-    .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    .domain([12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     .range([h - paddingBottom - paddingTop, 0]);
   let yAxis = d3
     .axisLeft(yScale)
@@ -106,7 +140,7 @@ d3.json ('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/ma
     .attr("y", 20)
     .text("Months");
 
-  // chart
+  // chart cells
 
   svg
     .append("g")
@@ -119,7 +153,7 @@ d3.json ('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/ma
     .attr('y', d => yScale(d.month))
     .attr('width', d => xScale.bandwidth(d.year))
     .attr('height', d => yScale.bandwidth(d.month))
-    .attr("fill", "grey")
+    .attr("fill", d => colorCalc(d.variance + 8.66))
     ;
 
 
