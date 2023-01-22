8abbba36b37ca3ae9512ec7eb6601787c6e1bc37
Vlad <37779807+vblanton@users.noreply.github.com>
2023-01-22 15:43:07 -0500

    begin chloropleth map

---
+++ freeCodeCamp/Data Visualization/US Educational Attainment Choropleth Map/index.js
@@ -0,0 +1,22 @@
+/**
+ * United States Educational Attainment Chloropleth Map
+ * 
+ * For this project: https://www.freecodecamp.org/learn/data-visualization/data-visualization-projects/visualize-data-with-a-choropleth-map
+ * Example: https://choropleth-map.freecodecamp.rocks/
+ * US Education Data:https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json
+ * US County Data:https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json
+ * 
+**/
+
+d3.json ('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
+  .then(data => {
+
+// Variables
+
+// Functions
+
+// Chloropleth Map
+
+
+  })
+  .catch(e => console.log(e));
