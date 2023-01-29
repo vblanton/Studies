55c2aa9e8a1c75d044e486e2ee868e03f42f66e6
Vlad <37779807+vblanton@users.noreply.github.com>
2023-01-29 15:18:39 -0500

    sql bike shop updates

--- freeCodeCamp/Relational Databases (Beta)/Bike Rental Shop/bike-shop.sh
+++ freeCodeCamp/Relational Databases (Beta)/Bike Rental Shop/bike-shop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 MAIN_MENU() {
   if [[ $1 ]]
@@ -16,7 +17,26 @@ esac
 }
 
 RENT_MENU() {
-
+#get available bikes
+AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available=true ORDER BY bike_id")
+#if no bikes available
+if [[ -z $AVAILABLE_BIKES ]]
+then
+#send to main menu
+MAIN_MENU "Sorry, we don't have any bikes available right now."
+else
+#display available bikes
+echo -e "\nHere are the bikes we have available:"
+echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+do
+  echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+done
+#ask for bike to rent
+echo -e "\nWhich one would you like to rent?"
+read BIKE_ID_TO_RENT
+#if input is not a number
+#send to main menu
+fi
 }
 
 RETURN_MENU() {
